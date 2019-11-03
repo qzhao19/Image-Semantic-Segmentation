a02c369eeec0296a5cada8e9df66b98c6650136f
Zhao Qi <qi.zhao1991@gmail.com>
2019-11-03 22:13:53 +0100

    Update tfrecords.py

--- DeepLabV3/utils/tfrecords.py
+++ DeepLabV3/utils/tfrecords.py
@@ -73,4 +73,68 @@ def write_tfrecords(file_path, tfrecords_file):
     tfrecord_writer.close()
     print('TF record file has been done')
   
+
+def _parse_tfrecords(serialized_example):
+    """Function focus on read tfrecords file
+    Args:
+        serialized_example: 
+    """
+
+    features = {'height': tf.FixedLenFeature([], tf.int32), 
+                'width': tf.FixedLenFeature([], tf.int32), 
+                'channel': tf.FixedLenFeature([], tf.int32),
+                'image': tf.FixedLenFeature([], tf.string),
+                'image_format': tf.FixedLenFeature([], tf.string),
+                'label': tf.FixedLenFeature([], tf.string),
+                'label_format': tf.FixedLenFeature([], tf.string)}
+
+    # get features from serialized tf example
+    parsed_features = tf.parse_single_example(serialized_example, features=features)
+
+    # get image height, width and channel value
+    height = parsed_features['height']
+    width = parsed_features['width']
+    channel = parsed_features['channel']
+
+    # get image data, set image type up float32
+    image = tf.io.decode_png(parsed_features['image'], channels=0)
+    image = tf.image.convert_image_dtype(image, dtype=tf.float32)
+    image.set_shape([None, None, 3])
+
+    # get label data, set label type up int32
+    label = tf.io.decode_png(parsed_features['label'], channels=1)
+    label = tf.image.convert_image_dtype(label, dtype=tf.int32)
+    label.set_shape([None, None, 1])
+
+    return image, label
+
+
+def read_tfrecords(tfrecords_file, shape, batch_size, num_images, num_epochs, mode='tf', is_training=True, is_shuffle=True):
+    """Read tfrecords file using tensorflow TFRecordDataset module, which means a tfrecord data generator
+    Args:
+        tfrecords_file: string, tfrecords file path
+        buffer_size: int, the number of example in the dataset, for perfect shuffling, a buffer size greater than or equal to the full size of the dataset is required
+        is_shuffle: boolean, shuffle dataset
+    Returns:
+        A tuple of images and labels one batch by one batch
+
+    """
+    # read tfrecord file
+    dataset = tf.data.TFRecordDataset(tfrecords_file)
+    if shuffle:
+        dataset = dataset.shffle(num_images)
+
+    dataset = dataset.map(_parse_tfrecords)
+    dataset = dataset.map(lambda image, label: preprocess_image(image, label, shape, mode, is_training))
+    dataset = dataset.prefetch(batch_size) # software pipelining
+
+    # We call repeat after shuffling, rather than before, to prevent separate epochs from blending together.
+    dataset = dataset.repeat(num_epochs)
+    dataset = dataset.batch(batch_size)
+
+    iterator = dataset.make_one_shot_iterator()
+    image, label = iterator.get_next()
+
+    return image, label
+
   
